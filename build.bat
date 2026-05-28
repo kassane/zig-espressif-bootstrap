32bf78f4410dbf1c330a73ce9d80ac7b0933bf89
Matheus Catarino <matheus-catarino@hotmail.com>
2026-05-28 15:18:19 -0300

    update build-scripts

--- build.bat
+++ build.bat
@@ -37,7 +37,7 @@ if "%VSCMD_ARG_HOST_ARCH%"=="x86" set OUTDIR=out-win-x86
 
 set ROOTDIR=%~dp0
 set "ROOTDIR_CMAKE=%ROOTDIR:\=/%"
-set ZIG_VERSION="0.16.0-xtensa"
+set ZIG_VERSION="0.17.0-xtensa"
 set JOBS_ARG=
 
 pushd %ROOTDIR%
